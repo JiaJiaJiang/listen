7298d6b20b29237c54aade3b1cab77daca77b909
luojia <luojia@luojia.me>
2019-03-25 18:55:53 +0800

    no message

--- listen.js
+++ listen.js
@@ -265,9 +265,9 @@ class Waterfall extends Visual{
 				dataArr[i*4]=dataArr[i*4+1]=dataArr[i*4+2]=0;
 				continue;
 			}
-			dataArr[i*4]=(v**2.9)*255;
-			dataArr[i*4+1]=(v**5)*255;
-			dataArr[i*4+2]=(v**1.8)*255;
+			dataArr[i*4]=(v**2.4)*255;
+			dataArr[i*4+1]=(v**4.3)*255;
+			dataArr[i*4+2]=(v**1.5)*255;
 		}
 		if(this.bufferMode)
 			this.bufferCtx.putImageData(this.freNewImageData,0,0);
